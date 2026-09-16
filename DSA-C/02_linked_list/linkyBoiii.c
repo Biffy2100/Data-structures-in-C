02964f0af800aa4faf442d769c640ab70dd6bd2e
vishal560 <biffyplaysmc@gmail.com>
2026-09-16 22:36:18 +0530

    ..

--- DSA-C/02_linked_list/linkyBoiii.c
+++ DSA-C/02_linked_list/linkyBoiii.c
@@ -1,3 +1,4 @@
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -23,13 +24,9 @@ void removeFirstNode(Node **head);
 void removeAtMiddle(Node *head, int pos);
 void removeLastNode(Node *head);
 
-/*
-TO BE IMPLEMENTED SOON
-void search();
-void length();
-void reverse();
-WORK IN PROGRESS
-*/
+int ListLength(Node *head);
+bool search(Node *head, int key);
+void reverseList(Node *head);
 
 void display(Node *head);
 
@@ -38,90 +35,143 @@ int main()
 
     int choice;
     Node *head = NULL;
-
-    printf("1.Insert at end\n");
-    printf("2.Insert at beginning\n");
-    printf("3.Insert at middle\n");
-
-    printf("4.Remove first node\n");
-    printf("5.Remove at middle\n");
-    printf("6.Remove last node\n");
-    printf("Select operation you want to perform: \n");
-
-    scanf("%d", &choice);
-
-    switch (choice)
+    while (ll == 0)
     {
-    case 1:
-    {
-        printf("Enter element: ");
-        int element;
+        printf("1.Insert at end\n");
+        printf("2.Insert at beginning\n");
+        printf("3.Insert at middle\n");
 
-        scanf("%d", &element);
+        printf("4.Remove first node\n");
+        printf("5.Remove at middle\n");
+        printf("6.Remove last node\n");
 
-        if (head == NULL)
+        printf("7.Search for an element\n");
+        printf("8.Find Length of linked list\n");
+        printf("9.Reverse the linked list\n");
+        printf("10.Display the list\n");
+        printf("11.Exit\n");
+        printf("Select operation you want to perform: \n");
+
+        scanf("%d", &choice);
+
+        switch (choice)
         {
-            head = createNode(element);
-        }
-        else
+        case 1:
         {
-            insertAtEnd(head, element);
+            printf("Enter element: ");
+            int element;
+
+            scanf("%d", &element);
+
+            if (head == NULL)
+            {
+                head = createNode(element);
+            }
+            else
+            {
+                insertAtEnd(head, element);
+            }
+            break;
         }
-        break;
-    }
 
-    case 2:
-    {
-        printf("Enter element: ");
-        int element;
-        scanf("%d", &element);
-
-        if (head == NULL)
+        case 2:
         {
-            head = createNode(element);
-        }
-        else
-        {
-            insertAtBeginning(&head, element);
-        }
-        break;
-    }
-    case 3:
-    {
-        printf("Enter element: ");
-        int element;
-        scanf("%d", &element);
+            printf("Enter element: ");
+            int element;
+            scanf("%d", &element);
 
-        break;
-    }
-    case 4:
-    {
-        break;
-    }
-    case 5:
-    {
-        break;
-    }
-    case 6:
-    {
-        break;
-    }
-    case 7:
-    {
-        break;
-    }
-    case 8:
-    {
-        break;
-    }
-    case 9:
-    {
-        break;
-    }
-    default:
-    {
-        break;
-    }
+            if (head == NULL)
+            {
+                head = createNode(element);
+            }
+            else
+            {
+                insertAtBeginning(&head, element);
+            }
+            break;
+        }
+        case 3:
+        {
+            printf("Enter element: ");
+            int element;
+            scanf("%d", &element);
+            printf("Enter position: ");
+            int pos;
+            scanf("%d", &pos);
+
+            if (ListLength(head) > pos)
+            {
+                printf("Invalid Position");
+                break;
+            }
+            else
+            {
+                insertAtMiddle(head, element, pos);
+            }
+
+            break;
+        }
+        case 4:
+        {
+            removeFirstNode(&head);
+            break;
+        }
+        case 5:
+        {
+            int pos;
+            printf("Enter position to remove element");
+            scanf("%d", &pos);
+
+            removeAtMiddle(head, pos);
+            break;
+        }
+        case 6:
+        {
+            removeLastNode(head);
+            break;
+        }
+        case 7:
+        {
+            int key;
+            printf("Enter element to search for: ");
+            scanf("%d", &key);
+
+            if (search(head, key))
+            {
+                printf("Element is present in list\n");
+            }
+            else
+            {
+                printf("Element not found\n");
+            }
+            break;
+        }
+        case 8:
+        {
+            printf("The length of the current list is %d\n", ListLength(head));
+            break;
+        }
+        case 9:
+        {
+            // to implement
+            // reverse()
+            break;
+        }
+        case 10:
+        {
+            display(head);
+            break;
+        }
+        case 11:
+        {
+            printf("Exiting the program thank you for traversing with us tonight!\n");
+            break;
+        }
+        default:
+        {
+            break;
+        }
+        }
     }
 
     return 0;
@@ -222,4 +272,36 @@ void display(Node *head)
         printf("%d\t", temp->element);
         temp = temp->next;
     }
+}
+
+int ListLength(Node *head)
+{
+    Node *temp = head;
+    int count = 0;
+    while (temp->next != NULL)
+    {
+        temp = temp->next;
+        count++;
+    }
+    return count;
+}
+
+bool search(Node *head, int key)
+{
+    Node *temp = head;
+    bool found = false;
+
+    while (temp->next != NULL)
+    {
+        if (temp->element == key)
+        {
+            found = true;
+            break;
+        }
+        else
+        {
+            temp = temp->next;
+        }
+    }
+    return found;
 }
